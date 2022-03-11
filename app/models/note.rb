7829c8d5d2f5af11c76f1aa8b15c00469302457d
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2022-03-11 15:19:06 +0000

    note no longer broadcasts remove to the notes-list for edits, view note updates via streams

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,8 @@
 class Note < ApplicationRecord
   belongs_to :user
 
+  # broadcasts_to self
+
   after_create_commit :broadcast_create_later
   after_destroy_commit :broadcast_destroy_later
   after_update_commit :broadcast_update_later
@@ -16,16 +18,17 @@ class Note < ApplicationRecord
   private
 
   def broadcast_create_later
-    broadcast_prepend_later_to user, target: 'notes-list', partial: 'notes/note-list-item', locals: {note: self}
+    broadcast_prepend_later_to user, target: 'notes-list', partial: 'notes/note-list-item', locals: { note: self }
   end
 
   def broadcast_destroy_later
   end
 
   def broadcast_update_later
-    broadcast_remove_to user
-    broadcast_prepend_later_to user, target: 'notes-list', partial: 'notes/note-list-item', locals: {note: self}
+    # broadcast_remove_to user
+    broadcast_prepend_later_to user, target: 'notes-list', partial: 'notes/note-list-item', locals: { note: self }
 
+    broadcast_replace_later_to self, target: self, partial: 'notes/note', locals: { note: self }
     # broadcast_replace_later_to user, target: self, partial: 'notes/note-list-item', locals: {note: self}
   end
 end
